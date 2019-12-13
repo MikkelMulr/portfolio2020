cf7d26a27e5b2d0e41c996bec0265bdb36a38a6f
Mike Mueller <muellerm.dev@gmail.com>
2019-12-13 08:15:18 -0500

    add contact component

---
+++ src/pages/contact.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './contact.styles.scss';
+
+export default function Contact() {
+  return (
+    <div className='Contact'>
+      <h2 className="Contact--header">Contact</h2>
+    </div>
+  );
+}

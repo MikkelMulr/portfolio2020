40e9bf65400964f94fe560ef7ac0f377b98c12de
Mike Mueller <muellerm.dev@gmail.com>
2020-01-14 13:50:36 -0500

    change to some about wording

--- src/pages/about.component.jsx
+++ src/pages/about.component.jsx
@@ -6,12 +6,11 @@ export default function About() {
 		<div className='About'>
 			<h2 className='About--header'>ABOUT ME</h2>
 			<p className='About--text'>
-				I'm Mike, a web developer and make of things from Toronto, Canada. A few years ago I decided to step away from
+				I'm Mike, a web developer from Toronto, Canada. A few years ago I decided to step away from
 				my career as a freelance illustrator to persue something I always felt was my real goal, programming. From a
 				humble "Hello World" to full fledged online websites and applications I used every step and stumble as a
 				learning experience to grow and build new things. The problem solving and creativity that are required for every
-				project bring a powerful sense of fullfilment and excitement that I have genuinely fallen in love with along the
-				way.
+				project bring a powerful sense of fullfilment and desire to keep growing.
 			</p>
 		</div>
 	);

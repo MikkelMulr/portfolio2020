89b2c622f67f07fa192bbd3940be2dce9931b84a
Mike.Mueller <michael17mueller@gmail.com>
2020-01-29 23:52:32 -0500

    additional updates to styles and expansion of portfolio section

--- src/pages/projects.component.jsx
+++ src/pages/projects.component.jsx
@@ -3,20 +3,21 @@ import ProjectCard from '../components/ProjectCard/ProjectCard.component';
 import './projects.styles.scss';
 
 export class Projects extends Component {
-
 	render() {
 		return (
 			<div className='Projects'>
-				<div className="Projects--Cards-container">
+				<div className='Projects--Cards-container'>
 					<div className='Projects--Cards'>
 						<ProjectCard title='Project' order='one' />
 						<ProjectCard title='Project' order='two' />
 						<ProjectCard title='Project' order='three' />
+						<ProjectCard title='Project' order='four' />
 					</div>
 					<div className='Projects--Cards'>
 						<ProjectCard title='Project' order='one' />
 						<ProjectCard title='Project' order='two' />
 						<ProjectCard title='Project' order='three' />
+						<ProjectCard title='Project' order='four' />
 					</div>
 				</div>
 			</div>

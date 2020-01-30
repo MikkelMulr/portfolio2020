b02db7ae45e194a5997b469f2642bb34fa57b7ca
Michael Mueller <muellerm.dev@gmail.com>
2020-01-30 15:23:50 -0500

    update to project cards, adding in content and icons

--- src/components/ProjectCard/ProjectCard.component.jsx
+++ src/components/ProjectCard/ProjectCard.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './ProjectCard.styles.scss';
 
-export default function ProjectCard({ title, order }) {
+
+
+export default function ProjectCard({ title, order, langs, bg }) {
+
+	let displayed = langs.map(lang => {
+		return <i className={`devicon-${lang}-plain`}></i>;
+	});
+
 	return (
-		<div className={`ProjectCard ${order}`}>
+		<div className={`ProjectCard ${order}`} >
 			<h3>{title}</h3>
-			<div className='ProjectCard--details' />
+			<div className='ProjectCard--details' style={{ backgroundImage: `url(${bg})` }}>
+				<div className="ProjectCard--details--icons">
+					{displayed}
+				</div>
+				<div className="ProjectCard--details--overlay"></div>
+			</div>
 		</div>
 	);
 }
--- src/pages/projects.component.jsx
+++ src/pages/projects.component.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ProjectCard from '../components/ProjectCard/ProjectCard.component';
+import Tymed from '../assets/images/tymed.png';
+import Avalon from '../assets/images/avalon.png';
 import './projects.styles.scss';
 
 export class Projects extends Component {
@@ -8,16 +10,16 @@ export class Projects extends Component {
 			<div className='Projects'>
 				<div className='Projects--Cards-container'>
 					<div className='Projects--Cards Cards-top'>
-						<ProjectCard title='Tymed' order='one' />
-						<ProjectCard title='Avalon Resilient' order='two' />
-						<ProjectCard title='Restaurant App' order='three' />
-						<ProjectCard title='OVBG PathFinder' order='four' />
+						<ProjectCard title='Tymed' order='one' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} bg={Tymed} />
+						<ProjectCard title='Avalon Resilient' order='two' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} bg={Avalon} />
+						<ProjectCard title='Restaurant App' order='three' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
+						<ProjectCard title='OVBG PathFinder' order='four' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
 					</div>
 					<div className='Projects--Cards'>
-						<ProjectCard title='OB Tracker' order='one' />
-						<ProjectCard title='Project TBD' order='two' />
-						<ProjectCard title='Project TBD' order='three' />
-						<ProjectCard title='Project TBD' order='four' />
+						<ProjectCard title='OB Tracker' order='one' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
+						<ProjectCard title='Project TBD' order='two' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
+						<ProjectCard title='Project TBD' order='three' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
+						<ProjectCard title='Project TBD' order='four' langs={['react', 'html5', 'css3', 'sass', 'nodejs', 'javascript']} />
 					</div>
 				</div>
 			</div>
